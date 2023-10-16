3154986c67bd5211e586f546a17a3db122223ab0
Harrison Goss <harrisonwgoss@gmail.com>
2023-10-16 14:40:35 -0700

    test commit

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifytest2": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifytest29dba7cee": {
       "AppClientID": "string",
